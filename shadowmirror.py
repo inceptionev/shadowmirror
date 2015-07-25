07bd703a45de8fa93c05177fd71c5e1ac0ca23b2
Edwin Chiu <inceptionev@gmail.com>
2015-07-25 09:13:17 +0000

    speeding up with generator

--- shadowmirror.py
+++ shadowmirror.py
@@ -6,15 +6,42 @@ import io
 import RPi.GPIO as GPIO
 import numpy as np
 from neopixel import *
+from fractions import Fraction
+import cv2
+#from PIL import Image
 
+def transfer():
+    stream = picamera.array.PiRGBArray(camera)
+    #stream=io.BytesIO()
+    for k in range(LOOP):
+        yield stream
+        stream.seek(0)
+        #data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+        #image = cv2.imdecode(data,1)
+        #image = Image.open(stream)
+        image=stream.array
+        #strip._led_data = image[:][30][1]
+	#print(image[1][1])
+        for i in range(strip.numPixels()):
+            if image[i][30][1] > THRESHOLD:
+                strip.setPixelColor(i, Color(0,0,255))
+            else:
+                strip.setPixelColor(i, Color(0,0,0))
+        strip.show()
+        stream.seek(0)
+        stream.truncate()
+        
 # LED strip configuration:
-LED_COUNT      = 72      # Number of LED pixels.
+LED_COUNT      = 320    # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
+#settings
+THRESHOLD = 190
+LOOP = 200
 # Create NeoPixel object with appropriate configuration.
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 # Intialize the library (must be called once before other functions).
@@ -22,34 +49,23 @@ strip.begin()
 
 #create and configure camera
 camera=picamera.PiCamera(sensor_mode=7)
-camera.resolution=(640,480)
-camera.framerate=80
+camera.resolution=(60,320)
+camera.framerate=90
 camera.iso = 400
-camera.start_preview(fullscreen=False,window=(120,1,640,480))
+#camera.shutter_speed = 12000
+camera.start_preview(fullscreen=False,window=(120,1,320,240))
 camera.led=False
 time.sleep(2)
 #lock in values
-camera.shutter_speed = camera.exposure_speed
-camera.exposure_mode = 'off'
 g = camera.awb_gains
+camera.exposure_mode = 'off'
 camera.awb_mode = 'off'
 camera.awb_gains = g
 
-
-stream=picamera.array.PiRGBArray(camera)
-for x in range(0,200):
-    camera.capture(stream, 'rgb', use_video_port=True)
-    #print('Captured %dx%d image' % (stream.array.shape[1], stream.array.shape[0]))
-    image=stream.array
-    print(image[1][1])
-    stream.seek(0)
-    for i in range(strip.numPixels()):
-        if image[320][i][1] > 64:
-            strip.setPixelColor(i, Color(0,0,32))
-        else:
-            strip.setPixelColor(i, Color(0,0,0))
-    strip.show()
-
+starttime = time.time()
+camera.capture_sequence(transfer(), 'rgb', use_video_port=True)
+#print('Captured %dx%d image' % (stream.array.shape[1], stream.array.shape[0]))
+print('Average: %.2f FPS' % (LOOP/(time.time()-starttime)))    
 
 camera.stop_preview()
 camera.close()
